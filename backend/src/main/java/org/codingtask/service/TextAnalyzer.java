92a3f13f3f86b8ee07fe672f848d4f70b3f9b81a
David Banyasz <1948523+dbanyasz@users.noreply.github.com>
2025-08-31 20:34:24 +0200

    unify analysis method

--- backend/src/main/java/org/codingtask/service/TextAnalyzer.java
+++ backend/src/main/java/org/codingtask/service/TextAnalyzer.java
@@ -1,6 +1,9 @@
 package org.codingtask.service;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * The program for calculating how many times letter in given sentence appears.
@@ -14,64 +17,59 @@ import java.util.HashMap;
 
 public class TextAnalyzer {
     public enum AnalysisMode {
-        VOWELS,
-        CONSONANTS
+        VOWELS {
+            @Override
+            public String filter(String input) {
+                return input.replaceAll("[^AEIOU]", "");
+            }
+
+            static final List<String> letters = List.of("A", "E", "I", "O", "U");
+
+            @Override
+            public List<String> letters() {
+                return letters;
+            }
+        },
+        CONSONANTS {
+            public String filter(String input) {
+                return input.replaceAll("[AEIOU]", "");
+            }
+
+            static final List<String> letters = List.of("B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z");
+
+            @Override
+            public List<String> letters() {
+                return letters;
+            }
+        };
+
+        public abstract String filter(String input);
+
+        public abstract List<String> letters();
     }
 
+
     public void analyze(AnalysisMode mode, String input) {
-        int numA = 0;
-        int numE = 0;
-        int numI = 0;
-        int numO = 0;
-        int numU = 0;
-        if (mode.equals(AnalysisMode.VOWELS)) {
-            char[] chars = input.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                if (chars[i] == 'a' || chars[i] == 'A')
-                    numA++;
-                if (chars[i] == 'e' || chars[i] == 'E')
-                    numE++;
-                if (chars[i] == 'i' || chars[i] == 'I')
-                    numI++;
-                if (chars[i] == 'o' || chars[i] == 'O')
-                    numO++;
-                if (chars[i] == 'u' || chars[i] == 'U')
-                    numU++;
-            }
-            System.out.println("Letter 'A' appears " + numA + " times");
-            System.out.println("Letter 'E' appears " + numE + " times");
-            System.out.println("Letter 'I' appears " + numI + " times");
-            System.out.println("Letter 'O' appears " + numO + " times");
-            System.out.println("Letter 'U' appears " + numU + " times");
-        } else if (mode.equals(AnalysisMode.CONSONANTS)) {
-            HashMap<String, Integer> consonants = new HashMap<>();
-            char[] chars = input.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                if (chars[i] != 'a'
-                        && chars[i] != 'A'
-                        && chars[i] != 'e'
-                        && chars[i] != 'E'
-                        && chars[i] != 'i'
-                        && chars[i] != 'I'
-                        && chars[i] != 'o'
-                        && chars[i] != 'O'
-                        && chars[i] != 'u'
-                        && chars[i] != 'U'
-                ) {
-                    String stringCharacter = String.valueOf(chars[i]).toUpperCase();
-                    if (consonants.containsKey(stringCharacter)) {
-                        Integer num = consonants.get(stringCharacter);
-                        num++;
-                        consonants.put(stringCharacter, num);
-                    } else {
-                        consonants.put(stringCharacter, 1);
-                    }
-                }
-            }
-            consonants.entrySet().forEach(entrySet -> {
-                System.out.println("Letter '" + entrySet.getKey() + "' appears " + entrySet.getValue() + " times");
-            });
+        String alphabeticalInput = input.toUpperCase().replaceAll("[^A-Z]", "");
+        String filteredInput = mode.filter(alphabeticalInput);
+
+        Map<String, Integer> letterOccurrences = mode.letters().stream().collect(Collectors.toMap(
+                letter -> letter,
+                letter -> 0
+        ));
+
+        char[] chars = filteredInput.toCharArray();
+        for (char aChar : chars) {
+            String stringCharacter = String.valueOf(aChar).toUpperCase();
+            letterOccurrences.computeIfPresent(stringCharacter, (letter, occurrences) -> occurrences + 1);
         }
+        letterOccurrences
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .forEach(entry -> {
+                    System.out.println("Letter '" + entry.getKey() + "' appears " + entry.getValue() + " times");
+                });
     }
 
     static void usage() {
